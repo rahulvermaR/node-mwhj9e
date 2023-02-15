3cff9e80ae86a79fc103cfbca48390d2c9e9a92c
rahulvermaR <94568339+rahulvermaR@users.noreply.github.com>
2023-02-15 16:47:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-mwhj9e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-mwhj9e)
---
+++ index.js
@@ -0,0 +1,13 @@
+//Write a program that prints numbers from 1 to 100. Except, for the multiples of three, print "google" and for the multiples of five, print "facebook". And, for the multiples of both, print "amazon".  Please answer with the stackblitz link.
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('amazon');
+  } else if (i % 3 === 0) {
+    console.log('google');
+  } else if (i % 5 === 0) {
+    console.log('facebook');
+  } else {
+    console.log(i);
+  }
+}
